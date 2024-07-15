3908bd6007f78c6c521fe0c17f0495100a118904
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2024-07-15 17:00:19 -0400

    Update stats.d.ts

--- calc/stats.d.ts
+++ calc/stats.d.ts
@@ -16,6 +16,7 @@ export declare const Stats: {
     calcStat(gen: Generation, stat: StatID, base: number, iv: number, ev: number, level: number, nature?: string): number;
     calcStatADV(natures: Natures, stat: StatID, base: number, iv: number, ev: number, level: number, nature?: string): number;
     calcStatRBY(stat: StatID, base: number, iv: number, level: number): number;
+    calcStatLGPE(natures: Natures, stat: StatID, base: number, iv: number, av: number, level: number, nature?: string): number;
     calcStatRBYFromDV(stat: StatID, base: number, dv: number, level: number): number;
     getHiddenPowerIVs(gen: Generation, hpType: HPTypeName): Partial<StatsTable<number>> | undefined;
     getHiddenPower(gen: Generation, ivs: StatsTable): {
