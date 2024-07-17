32804f00efa401004337876504e9c24ddf303dd9
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2024-07-17 15:40:35 -0400

    Update stats.js

--- calc/stats.js
+++ calc/stats.js
@@ -146,7 +146,7 @@ exports.Stats = new ((function () {
                     : mods[1] === stat
                         ? 0.9
                         : 1;
-            console.log(return Math.floor(((Math.floor(((base * 2 + iv) * level) / 100) + 5) * n * friendship) + av));
+            console.log(Math.floor(((Math.floor(((base * 2 + iv) * level) / 100) + 5) * n * friendship) + av));
             return Math.floor(((Math.floor(((base * 2 + iv) * level) / 100) + 5) * n * friendship) + av);
         }
     };
