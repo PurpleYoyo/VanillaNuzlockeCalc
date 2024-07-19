81f3a128f39391b807d553cca0aeefe268a3296d
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2024-07-18 20:00:31 -0400

    Update shared_controls.js

--- js/shared_controls.js
+++ js/shared_controls.js
@@ -1264,7 +1264,7 @@ function calcStat(poke, StatID) {
 	}
 	// Shedinja still has 1 max HP during the effect even if its Dynamax Level is maxed (DaWoblefet)
 	var total = calc.calcStat(gen, legacyStatToStat(StatID), base, ivs, evs, level, nature, avs, friendship);
-	console.log("3");
+	console.log("4");
 	console.log(StatID, total);
 	if (gen > 7 && StatID === "hp" && poke.isDynamaxed && total !== 1) {
 		total *= 2;
