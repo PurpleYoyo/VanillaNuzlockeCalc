b4f3e75811935fc35ac9c55a2592df6ef299273e
Purkat <43706820+PurpleYoyo@users.noreply.github.com>
2026-06-29 08:43:06 -0400

    remove some checks and rename

--- calc/calc.js
+++ calc/calc.js
@@ -7,8 +7,9 @@ var gen3_1 = require("./mechanics/gen3");
 var gen4_1 = require("./mechanics/gen4");
 var gen56_1 = require("./mechanics/gen56");
 var gen789_1 = require("./mechanics/gen789");
+var lgpe_1 = require("./mechanics/lgpe");
 var MECHANICS = [
-    function () { },
+    lgpe_1,
     gen12_1.calculateRBYGSC,
     gen12_1.calculateRBYGSC,
     gen3_1.calculateADV,
